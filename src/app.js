dc8b6a07c0fc6f81a594d736fd6be5573f8f77de
Shirley Silvana Suarez Startary <shirley.startary@gmail.com>
2020-06-09 23:14:28 -0500

    add styles and event draggable

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import GenderBread from './views/genderbread.js';
 const routerApp = () => {
   const container = document.getElementById('page-container');
   container.innerHTML = GenderBread.render();
+  GenderBread.afterrender()
 
 };
 
