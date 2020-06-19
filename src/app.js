9f6f796d9304859699605f7c0d0c6b27c6b12a31
Shirley Silvana Suarez Startary <shirley.startary@gmail.com>
2020-06-18 20:38:58 -0500

    add routes memory-game and puzzle

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import GenderBread from './views/genderbread.js';
+import MemoryGame from './views/memory-game.js';
+import GenderPuzzle from './views/puzzle.js'
+
+const route = [
+  {
+    path:'/',
+    component: GenderBread,
+  },
+  {
+    path:'/memory',
+    component: MemoryGame,
+  },
+  {
+    path:'/puzzle',
+    component: GenderPuzzle,
+  }
+  
+]
+const parseLocation = () => location.hash.slice(1).toLowerCase() || '/';
+
+const findComponentByPath = (path, routes) => routes.find(r => r.path.match(new RegExp(`^\\${path}$`, 'gm'))) || undefined;
 
 const routerApp = () => {
+  const path = parseLocation();
+  const { component = ErrorComponent } = findComponentByPath(path, route) || {};
   const container = document.getElementById('page-container');
-  container.innerHTML = GenderBread.render();
-  GenderBread.afterrender()
+  container.innerHTML = component.render();
+  component.afterrender()
 
 };
 
